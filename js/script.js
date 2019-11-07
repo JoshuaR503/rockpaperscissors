eba193d0834ebd77a6d5a74e4bdff7d5fb574226
Joshua <josshuagarciia000@gmail.com>
2019-11-06 18:02:02 -0800

    Rock Paper Scissors working

--- js/script.js
+++ js/script.js
@@ -1,57 +1,73 @@
-
+let computerScore = 0;
+let userScore = 0;
 
 // Return a random response.
 const computerInput = () => {
     const responses = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    const randomResponse = responses[randomNumber];
-
-    return randomResponse;
+    const randomNumber = Math.floor(Math.random() * 3);
+    return responses[randomNumber];
 }
 
-// Compares the responses and retuns a string with the result.
-const compareManager = (user, computer, choice) => {
+// Counter Manager
+const counterManager = (winner) => {
 
-    console.log(user);
-    console.log(computer);
-
-    if (user === choice && computer !== choice) {
-        return 'User wins';
-    } 
-
-    if (computer === choice && user !== choice) {
-        return 'Computer wins';
+    if (winner == 'computer') {
+        computerScore++;
+        
+        return `Computer wins`;
     }
 
-    if (computer == user) {
-        return 'No one wins';
+    if (winner == 'user') {
+        userScore++;
+
+        return `User wins`;
     }
+
+    return 'No one wins';
 }
 
 // Compares the responses and retuns the winer based on the selection.
 const compareResponses = (user, computer) => {
-    switch (user) {
-        case 'rock':
-            return compareManager(user, computer, 'rock');
-        case 'paper':
-            return compareManager(user, computer, 'paper');
-        case 'scissors':
-            return compareManager(user, computer, 'scissors');    
+
+    switch (user + " " + computer) {
+
+        case 'rock scissors':
+            return counterManager('user');
+        case 'scissors paper':
+            return counterManager('user');
+        case 'paper rock':
+            return counterManager('user');
+        
+        // TODO: find a way to reuse the code!
+        case 'scissors rocks':
+            return counterManager('computer');
+        case 'paper scissors':
+            return counterManager('computer');
+        case 'rock paper':
+            return counterManager('computer');
+
         default:
-            return 'Invalid Input';
+            return 'No one wins';
     }
 }
 
 // DOM Handler
-const domHandler = (user, computer) => {
+const domHandler = (user, computer, winner) => {
 
+    const counter = document.querySelector('#counter');
+
+    const result = document.querySelector('#result');
     const userMessage = document.querySelector('#userChoice');
     const computerMessage = document.querySelector('#computerChoice');
 
+    counter.innerHTML = `${userScore} - ${computerScore}`;
+
+    result.innerHTML = winner;
     userMessage.innerHTML = user;
     computerMessage.innerHTML = computer;
 }
 
+// CORE
 const userInput = document.querySelector('#input');
 const btn = document.querySelector('#shoot');
 
@@ -59,10 +75,11 @@ const btn = document.querySelector('#shoot');
 btn.addEventListener('click', () => {
 
     const user = input.value;
+
+    if (!user) return;
+
     const computer = computerInput();
-    const winner = compareResponses(user, computer);
+    const winner = compareResponses(user.toLowerCase(), computer);
 
-    domHandler(user, computer);
-
+    domHandler(user, computer, winner);
 });
-
